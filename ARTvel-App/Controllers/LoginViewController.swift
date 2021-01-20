3c15201349e9b3413c2350267c3de6f199b88cfa
Juan C <juanceballos@pursuit.org>
2021-01-20 03:24:24 -0500

    keyboard handling for login

--- ARTvel-App/Controllers/LoginViewController.swift
+++ ARTvel-App/Controllers/LoginViewController.swift
@@ -7,15 +7,16 @@
 
 import UIKit
 import FirebaseAuth
+import SnapKit
 
 class LoginViewController: UIViewController {
 
     let authSession = AuthSession()
     let db = DatabaseService()
-    //let mainTabVC = MainTabBarController(experience: <#T##String#>)
     let userExperienceVC = UserExperienceViewController()
-    
     let loginView = LoginView()
+    private var keyboardIsVisible = false
+    var originalYConstraint: CGFloat?
     
     override func loadView() {
         view = loginView
@@ -23,12 +24,77 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad()
+        registerForKeyboardNotifications()
         view.backgroundColor = UIColor(red: 51/255, green: 66/255, blue: 63/255, alpha: 1)
         loginView.passwordTextField.delegate = self
         loginView.usernameTextField.delegate = self
         loginView.createAccountButton.addTarget(self, action: #selector(createAccountButtonPressed), for: .touchUpInside)
         loginView.signedUpUserButton.addTarget(self, action: #selector(signinButtonPressed), for: .touchUpInside)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        unregisterForKeyboardNotifications()
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func unregisterForKeyboardNotifications()   {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        //print("keyboardWillShow")
+        
+        guard let keyboardFrame = notification.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect else  {
+            return
+        }
+        
+        if loginView.passwordTextField.isEditing {
+            moveKeyboardUp(keyboardFrame.size.height - 17)
+        }
+        else {
+            moveKeyboardUp(keyboardFrame.size.height)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        //print("keyboardWilllHide")
+    }
+    
+    private func moveKeyboardUp(_ height: CGFloat) {
+        if keyboardIsVisible { return }
+        originalYConstraint = height
+        loginView.signedUpUserButton.frame.origin.y -= height
+        print(loginView.signedUpUserButton.frame.origin.y)
+        print("\(height) height of key up")
+        loginView.createAccountButton.frame.origin.y -= 55
+        loginView.passwordTextField.frame.origin.y -= 55
+        loginView.usernameTextField.frame.origin.y -= 55
+        UIView.animate(withDuration: 0.5)   {
+            self.view.layoutIfNeeded()
+        }
+        
+        keyboardIsVisible = true
+    }
+    
+    private func resetUI()  {
+        keyboardIsVisible = false
+        loginView.signedUpUserButton.frame.origin.y += (originalYConstraint ?? 0)
+        print(loginView.signedUpUserButton.frame.origin.y)
+        print("\(originalYConstraint) og reset")
+        loginView.createAccountButton.frame.origin.y += 55
+        loginView.passwordTextField.frame.origin.y += 55
+        loginView.usernameTextField.frame.origin.y += 55
+        UIView.animate(withDuration: 1.0) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     private func signinExistingUser(email: String, password: String) {
@@ -115,8 +181,6 @@ class LoginViewController: UIViewController {
         signinExistingUser(email: email, password: password)
     }
     
-
-
 }
 
 extension LoginViewController: UITextFieldDelegate {
@@ -141,6 +205,7 @@ extension LoginViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+        resetUI()
         return true
     }
 }
